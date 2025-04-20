3d33a894e190ee11e5edf6ba06041736e0f9a3f0
KristiSeraj <serajkristi@gmail.com>
2025-04-20 15:03:37 +0200

    new exercises

---
+++ exercise_10/1-main.c
@@ -0,0 +1,15 @@
+// Deklaroni një matricë të tipit CHAR.
+// Numri i rreshtave jepet nga përdoruesi ndërsa numri i kolonave është 20.
+// Vlerat e saj merren nga përdoruesi me funksionin “scanf” (thirrje rresht pas rreshti).
+// Krijoni një funksion i cili merr në hyrje matricën e krijuar,
+// numri e rreshtave dhe një listë të tipit INT me gjatësi N.
+// Kjo listë duhet të popullohet me numrin e karaktereve
+// të përcjellë nga përdoruesi për çdo rresht me anë të funksionit “scanf”.
+#include <stdio.h>
+
+int main() {
+    int n;
+    char M[n][20];
+
+
+}
